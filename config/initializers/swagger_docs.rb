b8435b65bf544434bebe0b98b35d8c3a6517ee8b
Tim Schmelmer <tim.schmelmer@gmail.com>
2014-04-06 21:52:05 -0600

    Trying to fix the swagger JSON specs

--- config/initializers/swagger_docs.rb
+++ config/initializers/swagger_docs.rb
@@ -4,9 +4,9 @@ if Rails.env.development?
       # the extension used for the API
       :api_extension_type => :json,
       # the output location where your .json files are written to
-      :api_file_path => "public/api_docs/v1/",
+      :api_file_path => "public/api_docs/v1",
       # the URL base path to your API
-      :base_path => "tags-service-development.herokuapp.com/api/v1",
+      :base_path => "inventory-service-development.herokuapp.com/api/v1",
       # if you want to delete all .json files at each generation
       :clean_directory => true
     }
