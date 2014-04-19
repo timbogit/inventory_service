88c36363c457a26e503c8560f590cee8d18198c9
Tim Schmelmer <tim.schmelmer@gmail.com>
2014-04-19 16:16:08 -0600

    commenting out swagger docs initializer, as it will cause trouble deploying to heroku in development mode

--- config/initializers/swagger_docs.rb
+++ config/initializers/swagger_docs.rb
@@ -1,14 +1,14 @@
-if Rails.env.development?
-  Swagger::Docs::Config.register_apis({
-    "1.0" => {
-      # the extension used for the API
-      :api_extension_type => :json,
-      # the output location where your .json files are written to
-      :api_file_path => "public/api_docs/v1",
-      # the URL base path to your API
-      :base_path => "inventory-service-development.herokuapp.com/api/v1",
-      # if you want to delete all .json files at each generation
-      :clean_directory => true
-    }
-  })
-end
+# if Rails.env.development?
+#   Swagger::Docs::Config.register_apis({
+#     "1.0" => {
+#       # the extension used for the API
+#       :api_extension_type => :json,
+#       # the output location where your .json files are written to
+#       :api_file_path => "public/api_docs/v1",
+#       # the URL base path to your API
+#       :base_path => "inventory-service-development.herokuapp.com/api/v1",
+#       # if you want to delete all .json files at each generation
+#       :clean_directory => true
+#     }
+#   })
+# end
