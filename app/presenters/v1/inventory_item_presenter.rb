c7e6a1559caa6d72c331821acdb4407a3d2ce0d2
Tim Schmelmer <tim.schmelmer@gmail.com>
2014-04-21 15:40:56 -0600

    Adding random descriptions, as well as the item IDs to the item representation

--- app/presenters/v1/inventory_item_presenter.rb
+++ app/presenters/v1/inventory_item_presenter.rb
@@ -18,6 +18,19 @@ class Presenters::V1::InventoryItemPresenter < ::Presenter
     "https://a0.lscdn.net/imgs/b286fd13-c9b0-4c66-90f7-3c20aaa22f9d/540_q80.jpg",
   ]
 
+  DESCRIPTIONS = [
+    "Lorem ipsum dolor sit amet, an electram constituto consequuntur mea. Vix commune pertinax perpetua te. Scriptorem necessitatibus nec ad, alia audire an pri. Inani vocibus scripserit ei has. Duo at delicata molestiae, in modus malis postulant eam, odio persius gubergren sit ne.",
+    "Ea vix mollis impetus consequat, eum aperiam patrioque adversarium no. Cotidieque consequuntur ei his, accumsan consectetuer te pro. Ei labore singulis his, vis an nominavi qualisque vituperata. Eum ocurreret iracundia comprehensam ne, at eam mucius animal. Et case labitur maiestatis vix, unum nulla reprimique et eum. Percipitur efficiantur instructior at mel, sed prompta numquam percipitur at.",
+    "Duis iudico ei duo, posse corpora id vel. Nam nemore malorum splendide at. Mel stet assueverit necessitatibus ad, vix id atqui tollit partem. Eu causae quaestio mea, ex deseruisse adipiscing cum, has id eleifend corrumpit.",
+    "Illud graecis cu nec, an mea albucius accusata philosophia, ei omnes facilis epicuri sed. Id sed tation deleniti. Everti pericula est ei, at aperiri accommodare est. Alii dicat referrentur eos at, ea unum voluptatum per. Vix verear oportere ei, per ne rebum ullum alterum.",
+    "Duo tincidunt appellantur ex, et nisl omittam nam. Ad putent tamquam eum, ceteros neglegentur vis an, et eos probo posse corrumpit. Facer vivendum no sit, accusam invenire duo eu. Vocent lobortis eu sea, no volutpat dignissim conclusionemque duo, in voluptua inciderint sit. Sit tota dolorum officiis ut, quando numquam ea pri. No vix quot oporteat, et aperiam suscipit imperdiet usu.",
+    "Quis soluta vituperatoribus at per, platonem persequeris mediocritatem in pro, sed dolorum ponderum an. Ne dico similique quo, quis hendrerit vis ex. Nec in iriure tamquam complectitur, et vel quot adhuc volumus, esse eros consectetuer eu quo. Ea iusto assueverit eam, ad eum soleat impetus. Mollis laoreet imperdiet ius ei. Munere complectitur ea nec, alii complectitur contentiones ea duo.",
+    "Ne his causae laoreet appareat, at nam dolores delicata, nec cu eirmod definitionem. In iriure aliquid incorrupte pri. Mel epicuri reformidans ex. His an partem integre aliquando, ad aliquando constituto has, ei cum iisque detraxit insolens. Unum errem convenire in est, sed no malis doming accusamus. No mel modus legimus appareat.",
+    "Alia volutpat id has, te pro adhuc dicant libris, qui an ancillae antiopam. Has tempor molestie in. Ei postea luptatum legendos vix, in commodo utroque atomorum sea. Altera expetendis necessitatibus ea pro, agam erant mollis cum cu. Eu qui stet munere, omittam delicatissimi te est.",
+    "Per euripidis definitiones ei, duo cu elit singulis. Equidem inciderint est ut, inimicus aliquando intellegat his ex, ex est verterem splendide. At vim dolores assueverit. Stet labores qualisque in eam, eu per case mutat. Oportere delicatissimi qui in, ex vim quem velit invenire.",
+    "No qui aliquip detraxit senserit, case iracundia expetendis duo ex, an cibo dicam semper mei. Pri dico fastidii vivendum no, quidam noster intellegam ius te. Illud blandit vel ei, cu mea meis utamur accusam, alia hendrerit an duo. Te his nullam detracto, oratio feugiat mandamus has ad."
+  ]
+
   def initialize(item)
     @inventory_item = item
     super(@inventory_item)
@@ -25,9 +38,11 @@ class Presenters::V1::InventoryItemPresenter < ::Presenter
 
   def to_hash(item = inventory_item)
     HashWithIndifferentAccess.new({
+                                    id:           item.id,
                                     full_title:   item.title,
                                     short_title:  truncate(item.title, length: 15),
                                     image_url:    IMAGE_URLS[item.id % IMAGE_URLS.size],
+                                    description:  DESCRIPTIONS[item.id % DESCRIPTIONS.size],
                                     usd:          number_to_currency(item.price, :unit => "$"),
                                     city:         { id: item.city_id, name: item.city.name },
                                     path:         inventory_item_path(self.class.version_number, item.id),
