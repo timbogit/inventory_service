321f433cd6c103eed246013b410121a60719d0ac
Tim Schmelmer <tim.schmelmer@gmail.com>
2014-04-21 15:40:56 -0600

    Adding random descriptions, as well as the item IDs to the item representation

--- app/presenters/v1/full_inventory_item_presenter.rb
+++ app/presenters/v1/full_inventory_item_presenter.rb
@@ -2,9 +2,11 @@ class Presenters::V1::FullInventoryItemPresenter < ::InventoryItemPresenter
 
   def to_hash(item = inventory_item)
     HashWithIndifferentAccess.new({
+                                    id:           item.id,
                                     full_title:   item.title,
                                     short_title:  truncate(item.title, length: 15),
                                     image_url:    IMAGE_URLS[item.id % IMAGE_URLS.size],
+                                    description:  DESCRIPTIONS[item.id % DESCRIPTIONS.size],
                                     usd:          number_to_currency(item.price, :unit => "$"),
                                     city:         { id: item.city_id, name: item.city.name },
                                     path:         inventory_item_path(self.class.version_number, item.id),
--- app/presenters/v1/inventory_item_presenter.rb
+++ app/presenters/v1/inventory_item_presenter.rb
@@ -2,9 +2,11 @@ class Presenters::V1::InventoryItemPresenter < ::InventoryItemPresenter
 
   def to_hash(item = inventory_item)
     HashWithIndifferentAccess.new({
+                                    id:           item.id,
                                     full_title:   item.title,
                                     short_title:  truncate(item.title, length: 15),
                                     image_url:    IMAGE_URLS[item.id % IMAGE_URLS.size],
+                                    description:  DESCRIPTIONS[item.id % DESCRIPTIONS.size],
                                     usd:          number_to_currency(item.price, :unit => "$"),
                                     city_uri:     "http://#{RemoteCity.host}/api/v#{RemoteCity.api_version}/#{RemoteCity.resources}/#{item.city_id}",
                                     path:         inventory_item_path(self.class.version_number, item.id),
